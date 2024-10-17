85e5f4bd2b9073952440dc459dd6ab0bcafe6875
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2024-10-17 13:52:14 +0700

    menambahkan fungsi button tambah

--- gui.py
+++ gui.py
@@ -1,13 +1,42 @@
+# import function dari folder module
 from modules import functions
+# import freesimplegui
 import FreeSimpleGUI as sg
 
+# tema warna aplikasi todo
 sg.theme('black')
 
+# untuk label tex
 label = sg.Text("Masukkan ToDo anda")
-input_box = sg.InputText(tooltip="Masukkan ToDo")
+# input box
+input_box = sg.InputText(tooltip="Masukkan ToDo", key="todo")
+# button
 button_tambah = sg.Button("Tambah")
 
-window = sg.Window('Aplikasi ToDo', layout=[[label],[input_box,button_tambah]])
+# window aplikasi atau tampilan keseluruhan
+window = sg.Window('Aplikasi ToDo',
+                   layout=[[label],[input_box,button_tambah]],
+                   font=('Helvetica', 14))
 
-window.read()
+# fungsi looping menggunakan while
+while True:
+    # event=button"tambah", value=keytodo'
+    event, values = window.read()
+    print(event)
+    print(values)
+    # kondisi event=button yang di klik
+    match event:
+        # jika button tambah yg ditekan fungsi dijalankan
+        case "Tambah":
+            # untuk mengambil fungsi get_todos berisi 'r' pada function
+            todos = functions.get_todos()
+            # key berisitodo pada input box yaitu values
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            # untuk mengambil fungsi write_todos berisi 'w' pada function
+            functions.write_todos(todos)
+        # fungsi menutup window agar tidak error
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
